113ac7980aaff7994cdc85954286c0c61af5e974
Henry Zeng <youhengzeng8@gmail.com>
2024-08-06 20:40:39 -0400

    second commit

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const About = () => {
               Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
               sapiente odio unde dolor?
             </p>
-            <a href="#" className="btn">
+            <a href="www.google.com" className="btn">
               read more
             </a>
           </article>
